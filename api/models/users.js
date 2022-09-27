ad391ace6bbc0bcba8742f8d0438bc0f5160110d
ryand913 <98771607+ryand913@users.noreply.github.com>
2022-09-27 10:59:50 -0400

    Corrected file adds

---
+++ api/models/users.js
@@ -0,0 +1,78 @@
+'use strict';
+const {
+  Model, DataTypes
+} = require('sequelize');
+
+var bcrypt = require('bcryptjs');
+module.exports = (sequelize) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  //define a User attribtues
+  User.init({
+    firstName:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          notNull: {
+            msg: "A first name is required"
+          }
+        }
+    },
+    lastName:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          notNull: {
+            msg: "A last name is required"
+          }
+        }
+    },
+    emailAddress:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: 'Email address already in use!'
+        },
+        isEmail: true,
+        validate:{
+          notNull: {
+            msg: "An email address is required"
+          },
+          isEmail: {
+            msg: 'Please provide a valid email address'
+          }
+        }
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        set(val){
+          const hashPassword = bcrypt.hashSync(val,10);
+          this.setDataValue('password', hashPassword)
+        },
+        validate: {
+          notNull: {
+            msg: 'A password is required'
+          }
+        }
+    },
+}, {
+    sequelize
+  });
+    User.associate = (models) => {
+    User.hasMany(models.Course, {
+      foreignKey: {
+        fieldName: 'userId',
+      },
+    });
+  };
+  return User;
+};
